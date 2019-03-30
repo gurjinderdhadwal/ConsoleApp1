a56aaafae4d1f99451ab3714b9619c253f959394
visualstudio <visualstudio@122-PC22>
2019-03-29 21:56:02 -0400

    final commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace c0730228
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" Downloading a file");
+            Download();
+            Console.ReadLine();
+        }
+
+        static async void Download()
+        {
+            await Network.Download();
+            Console.WriteLine("Download Complete");
+        }
+
+    }
+
+    class Network
+    {
+        static ArrayList WbPageContents = new ArrayList();
+        public static async Task Download()
+        {
+
+            HttpClient client = new HttpClient();
+            var data = await client.GetStringAsync("http://ibm.com");
+            Console.WriteLine(data);
+            foreach (var i in data)
+            {
+                WbPageContents.Add(i);
+            }
         }
     }
 }
+
